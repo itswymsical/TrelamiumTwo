a9ad6419f4f267f8ec0e01b76e4bb94dc8f5f8f5
Nakamura <60052047+naakaamura@users.noreply.github.com>
2021-04-04 23:54:42 -0300

    Moved weak references to a separate class

--- Trelamium2.cs
+++ Trelamium2.cs
@@ -1,5 +1,6 @@
 using Terraria;
 using Terraria.ModLoader;
+using Trelamium2.Core;
 
 namespace Trelamium2
 {
@@ -22,51 +23,24 @@ namespace Trelamium2
             };
         }
 
-        public override void Load()
-        {
-            Mod yabhb = ModLoader.GetMod("FKBossHealthBar");
-            if (yabhb != null)
-            {
-                yabhb.Call("hbStart");
-
-                yabhb.Call("hbSetTexture",
-                    GetTexture("UI/OmikBarLeft"),
-                    GetTexture("UI/OmikBarMid"),
-                    GetTexture("UI/OmikBarRight"),
-                    GetTexture("UI/OmikBarFill"));
-
-                yabhb.Call("hbFinishMultiple",
-                    NPCType("AzolinthHead"),
-                    NPCType("AzolinthBody"),
-                    NPCType("AzolinthTail"));
-            }
-        }
-
         public override void Unload()
         {
             if (!Main.dedServ)
                 Instance = null;
         }
 
-        public override void PostSetupContent()
-        {
-            Mod bossChecklist = ModLoader.GetMod("BossChecklist");
-            if (bossChecklist != null)
-            {
+        public override void PostSetupContent() => WeakReferences.PerformModSupport();
 
-            }
-        }
-
-        public override void UpdateMusic(ref int music, ref MusicPriority priority)
+        /* public override void UpdateMusic(ref int music, ref MusicPriority priority)
         {
-            /*if (Main.myPlayer != -1 && !Main.gameMenu && Main.LocalPlayer.active)
+            if (Main.myPlayer != -1 && !Main.gameMenu && Main.LocalPlayer.active)
             {
                 /*if (Main.LocalPlayer.GetModPlayer<TrelamiumModPlayer>().ZoneDruidsGarden)
                 {
                     music = GetSoundSlot(SoundType.Music, "Sounds/Music/DruidsGarden");
                     priority = MusicPriority.BiomeHigh;
                 }
-            }*/
-        }
+            }
+        } */
     }
 }
