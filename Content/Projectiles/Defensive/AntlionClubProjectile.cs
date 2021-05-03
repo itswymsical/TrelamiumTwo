48671dab15c458e481c6427eaf7c61566d12e98a
iNxSignia <cobaltdragon13@Outlook.com>
2021-05-03 11:42:40 -0700

    Weapons & More Heavies

---
+++ Content/Projectiles/Defensive/AntlionClubProjectile.cs
@@ -0,0 +1,145 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using TrelamiumTwo.Common.Extensions;
+
+namespace TrelamiumTwo.Content.Projectiles.Melee // Eldrazi Code imported from EH
+{
+	public sealed class AntlionClubProjectile : ModProjectile
+	{
+        public override string Texture => "TrelamiumTwo/Content/Items/Weapons/Heavies/AntlionClub";
+        private enum AIState
+		{
+			Spawning,
+			Swinging = 2
+		}
+		AIState State
+		{
+			get => (AIState)projectile.ai[0];
+			set => projectile.ai[0] = (int)value;
+		}
+		private readonly float MaxChargeTime = 40f;
+
+		private float RotationStart => MathHelper.PiOver2 + (projectile.direction == -1 ? MathHelper.Pi : 0);
+		private float RotationOffset => projectile.direction == 1 ? 0 : MathHelper.PiOver2;
+
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.TrailingMode[projectile.type] = 2;
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 6;
+		}
+		public override void SetDefaults()
+		{
+			projectile.width = 50;
+			projectile.height = 50;
+
+			projectile.penetrate = -1;
+
+			projectile.melee = true;
+			projectile.friendly = true;
+			projectile.tileCollide = false;
+			projectile.netImportant = true;
+			projectile.ownerHitCheck = true;
+			projectile.manualDirectionChange = true;
+		}
+
+		public override bool PreAI()
+		{
+			Player owner = Main.player[projectile.owner];
+			if (!owner.active || owner.dead)
+			{
+				projectile.Kill();
+			}
+
+			if (State == AIState.Swinging)
+			{
+				projectile.ai[1] -= 4;
+
+				if (projectile.ai[1] <= 0)
+				{
+					projectile.Kill();
+				}
+			}
+			else
+			{
+				if (++projectile.ai[1] >= MaxChargeTime)
+				{
+					projectile.ai[1] = MaxChargeTime;
+					Main.LocalPlayer.GetModPlayer<Common.Players.TrelamiumPlayer>().shakeEffects = .015f;
+				}
+
+				if (Main.myPlayer == projectile.owner && !owner.channel && projectile.ai[1] >= (MaxChargeTime / 2))
+				{
+					State = AIState.Swinging;
+					projectile.netUpdate = true;
+				}
+			}
+
+			SetProjectilePosition(owner);
+
+			SetOwnerAnimation(owner);
+
+			return (false);
+		}
+
+		public override bool CanDamage()
+			=> State != AIState.Spawning;
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			Main.PlaySound(SoundID.DD2_MonkStaffGroundImpact, -1, -1);
+			Core.Utils.DustUtils.SpawnDustCloud(projectile.position, projectile.width, projectile.height, 32, 60);
+		}
+        public override void Kill(int timeLeft)
+        {
+			Main.PlaySound(SoundID.DD2_MonkStaffGroundImpact, -1, -1);
+			Core.Utils.DustUtils.SpawnDustCloud(projectile.position, projectile.width, projectile.height, 32, 50);
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			if (State == AIState.Swinging)
+			{
+				this.DrawProjectileTrailCentered(spriteBatch, lightColor, 0.4f, 0.15f);
+			}
+
+			return this.DrawProjectileCentered(spriteBatch, lightColor);
+		}
+
+		private void SetProjectilePosition(Player owner)
+		{
+			projectile.spriteDirection = projectile.direction;
+
+			Vector2 rotatedPoint = owner.RotatedRelativePoint(owner.MountedCenter);
+
+			projectile.rotation = RotationStart - (MathHelper.Pi / MaxChargeTime * projectile.ai[1]) * projectile.direction;
+			projectile.Center = rotatedPoint + (projectile.rotation - MathHelper.PiOver4 - RotationOffset).ToRotationVector2() * 64;
+		}
+
+		private void SetOwnerAnimation(Player owner)
+		{
+			owner.itemTime = owner.itemAnimation = 10;
+
+			owner.heldProj = projectile.whoAmI;
+
+			float currentAnimationFraction = projectile.ai[1] / MaxChargeTime;
+
+			if (currentAnimationFraction < 0.4f)
+			{
+				owner.bodyFrame.Y = owner.bodyFrame.Height * 3;
+			}
+			else if (currentAnimationFraction < 0.75f)
+			{
+				owner.bodyFrame.Y = owner.bodyFrame.Height * 2;
+			}
+			else
+			{
+				owner.bodyFrame.Y = owner.bodyFrame.Height;
+			}
+		}
+	}
+}
