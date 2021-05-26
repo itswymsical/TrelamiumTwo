2da196707cc1f119d3715e2621a9eb967ad2fdec
Lemmy-Koopa <kennithvolcy@gmail.com>
2021-05-26 15:47:10 -0400

    Added - MarbleStaff

---
+++ Content/Items/Weapons/Magic/MarbleStaff.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TrelamiumTwo.Content.Items.Weapons.Magic
+{
+    public class MarbleStaff : ModItem
+    {
+
+        public override void SetStaticDefaults() => DisplayName.SetDefault("Marble Staff");
+
+        public override void SetDefaults()
+        {
+            Item.staff[item.type] = true;
+            item.magic = true;
+            item.noMelee = true;
+            item.autoReuse = true;
+
+            item.damage = 23;
+            item.knockBack = 2f;
+            item.useTime = item.useAnimation = 23;
+
+            item.rare = ItemRarityID.White;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.UseSound = SoundID.Item9;
+
+            item.shoot = ModContent.ProjectileType<Projectiles.Magic.MarbleProjectile>();
+            item.shootSpeed = 16f;
+
+            item.value = Item.buyPrice(silver: 80);
+
+            item.width = item.height = 50;
+            
+
+        }
+    }
+}
---
+++ Content/Projectiles/Magic/MarbleProjectile.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TrelamiumTwo.Content.Projectiles.Magic
+{
+    public class MarbleProjectile : ModProjectile
+    {
+        
+        public override void SetStaticDefaults() => DisplayName.SetDefault("Marble Projectile");
+
+        public override string Texture => "Terraria/Projectile_" + ProjectileID.None;
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 16;
+
+            projectile.friendly = true;
+            projectile.aiStyle = -1;
+            projectile.tileCollide = false;
+            projectile.hide = true;
+            projectile.magic = true;
+        }
+        
+
+        int Timer = 0;
+        public override void AI()
+        {
+            if (projectile.scale <= 3) // this scales the projectiles util it scales the projectile until its 3 times the original projectile
+            {
+                projectile.scale *= 1.03f;
+                projectile.width = (int)(projectile.scale * 20); // make the width hitbox bigger
+                projectile.height = (int)(projectile.scale * 24);  // makes the height hitbox bigger
+            }
+            var dust = Dust.NewDustDirect(projectile.position, 1, 1, 159, 0, 0, 100, Color.White, 2f);
+            dust.noGravity = true;
+            dust.velocity *= 0;
+            dust.scale = 0.97f;
+
+            float t = (float)Main.time * 0.1f;
+            dust.position = projectile.Center + new Vector2((float)Math.Sin(2 * t) / 2f, -(float)Math.Cos(2 * t) / 3f) * 100f;
+
+            
+        }
+    }
+}
